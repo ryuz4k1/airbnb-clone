9049915786bc5baa60db117430e39cccf6efcd1b
ryuz4k1 <ozerr.murat@hotmail.com>
2022-10-30 00:17:44 +0300

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+import "../styles/globals.css";
 
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routerChangeComplete", progress.finish);
+Router.events.on("routerChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
